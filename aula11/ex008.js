927e836f05c10c98a9e04380dc8bf89666d7bda7
João Carlos <joaojnr10@gmail.com>
2026-02-07 00:19:02 -0300

    aula11
    concluir mais alguns exercícios e 1 desafio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59.05
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida da via. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
