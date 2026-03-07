d024cb58e8df4bb293cfb79e8e1d768b478a55bd
João Carlos <joaojnr10@gmail.com>
2026-03-07 14:35:19 -0300

    Revert "aula11"
    This reverts commit 927e836f05c10c98a9e04380dc8bf89666d7bda7.

--- aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 59.05
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-if(vel > 60) { //Condição Simples!
-    console.log(`Você ultrapassou a velocidade permitida da via. MULTADO!`)
-}
-console.log(`Dirija sempre usando cinto de segurança!`)
